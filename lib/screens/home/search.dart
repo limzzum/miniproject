a2d2a8a50bcda369b919749fc0669ede386a33ac
limzzum <chfhddl1234@naver.com>
2022-02-14 19:28:07 +0900

    search 기능 추가

--- lib/screens/home/search.dart
+++ lib/screens/home/search.dart
@@ -6,69 +6,106 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'detail_poster.dart';
 
-class Search extends SearchDelegate{
-
-  var controller=Get.put(Controller());
-  @override
-  List<Widget>? buildActions(BuildContext context) {
-
-  }
+class Search extends SearchDelegate {
+  var controller = Get.put(Controller());
 
   @override
-  Widget? buildLeading(BuildContext context) {
+  List<Widget>? buildActions(BuildContext context) {}
 
-  }
+  @override
+  Widget? buildLeading(BuildContext context) {}
 
   @override
   Widget buildResults(BuildContext context) {
     return FutureBuilder(
         future: controller.loadSearchResult(query),
-        builder: (data,snapshot){
-          if(snapshot.hasData==false)
-            return Center(child: CircularProgressIndicator());
-          else
-            if(controller.searchResult.isEmpty)
-              return Text('noData'.tr);
-            else {
-              return Container(
+        builder: (data, snapshot) {
+          if (snapshot.hasData == false) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (controller.searchResult.isEmpty) {
+            return Text('noData'.tr);
+          } else {
+            return Container(
                 width: KEY.width.w,
                 alignment: Alignment.center,
-                padding: EdgeInsets.only(left: 20.w,top:20.w),
-              child:GridView.count(
-                childAspectRatio: 0.8,
-                mainAxisSpacing: 20.w,
-                crossAxisCount: 3,
-                children: List.generate(controller.searchResult.length, (index)
-                => GestureDetector(
-                  onTap: () {
-                    Get.to(()=> DetailPosterPage(id:controller.searchResult[index].id));
-                  },
-                  child: Container(
-                      clipBehavior: Clip.hardEdge,
-                      margin: EdgeInsets.only(right: 20.w),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5.w),
-                      ),
-                      child: controller
-                          .searchResult[index]
-                          .image
-                          .contains('http')
-                          ? Image.network(
-                          controller.searchResult[index].image,
-                          fit: BoxFit.cover)
-                          : Image.network(
-                          "http://www.kopis.or.kr" +
-                              controller.searchResult[index].image,
-                          fit: BoxFit.cover)),
-                )
-              )
-    ));
-            }});
-    }
+                padding: EdgeInsets.only(left: 20.w, top: 20.w),
+                child: GridView.count(
+                    childAspectRatio: 0.8,
+                    mainAxisSpacing: 20.w,
+                    crossAxisCount: 3,
+                    children: List.generate(
+                        controller.searchResult.length,
+                        (index) => GestureDetector(
+                              onTap: () {
+                                Get.to(() => DetailPosterPage(
+                                    id: controller.searchResult[index].id));
+                              },
+                              child: Container(
+                                  clipBehavior: Clip.hardEdge,
+                                  margin: EdgeInsets.only(right: 20.w),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5.w),
+                                  ),
+                                  child: controller.searchResult[index].image
+                                          .contains('http')
+                                      ? Image.network(
+                                          controller.searchResult[index].image,
+                                          fit: BoxFit.cover)
+                                      : Image.network(
+                                          "http://www.kopis.or.kr" +
+                                              controller
+                                                  .searchResult[index].image,
+                                          fit: BoxFit.cover)),
+                            ))));
+          }
+        });
+  }
 
   @override
   Widget buildSuggestions(BuildContext context) {
+    if(query.isNotEmpty){
+    return FutureBuilder(
+        future: controller.loadSearchResult(query),
+        builder: (data, snapshot) {
+          if (snapshot.hasData == false) {
+            return Container();//const Center(child: CircularProgressIndicator());
+          } else if (controller.searchResult.isEmpty) {
+            return Text('noData'.tr);
+          } else {
+            return Container(
+                width: KEY.width.w,
+                alignment: Alignment.center,
+                padding: EdgeInsets.only(left: 20.w, top: 20.w),
+                child: GridView.count(
+                    childAspectRatio: 0.8,
+                    mainAxisSpacing: 20.w,
+                    crossAxisCount: 3,
+                    children: List.generate(
+                        controller.searchResult.length,
+                            (index) => GestureDetector(
+                          onTap: () {
+                            Get.to(() => DetailPosterPage(
+                                id: controller.searchResult[index].id));
+                          },
+                          child: Container(
+                              clipBehavior: Clip.hardEdge,
+                              margin: EdgeInsets.only(right: 20.w),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5.w),
+                              ),
+                              child: controller.searchResult[index].image
+                                  .contains('http')
+                                  ? Image.network(
+                                  controller.searchResult[index].image,
+                                  fit: BoxFit.cover)
+                                  : Image.network(
+                                  "http://www.kopis.or.kr" +
+                                      controller
+                                          .searchResult[index].image,
+                                  fit: BoxFit.cover)),
+                        ))));
+          }
+        });}
     return Container();
   }
-
 }
